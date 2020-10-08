bcccebea30066321aa8ccda5394cbb0296736f8d
cjaewon <jwn4492@outlook.com>
2020-10-08 22:38:37 +0900

    Create User Command

--- cmd/bootstrap.go
+++ cmd/bootstrap.go
@@ -1,5 +1,6 @@
 package cmd
 
+// cmd = commands list, not a conventional concept in golang
 // Bootstrap : Bootstrap All Commands
 func Bootstrap() {
 	book()
--- cmd/user.go
+++ cmd/user.go
@@ -16,5 +16,19 @@ func user() {
 }
 
 func signUp(s *discordgo.Session, m *discordgo.MessageCreate) {
+	embed := discordgo.MessageEmbed{
+		Title:       "계정 생성",
+		Description: "계정을 생성하시면 lang.pkg의 다양한 기능을 이용하실 수 있어요.",
+		Fields: []*discordgo.MessageEmbedField{
+			{
+				Name:  "약관동의",
+				Value: "✅ 이모지를 눌러주시면 [약관](http://example.org/)에 동의하고 가입이 됩니다.",
+			},
+		},
+	}
+
+	embedMsg, _ := s.ChannelMessageSendEmbed(m.ChannelID, &embed)
+
+	s.MessageReactionAdd(embedMsg.ChannelID, embedMsg.ID, "✅")
 
 }
